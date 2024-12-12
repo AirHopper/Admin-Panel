26bd7be478e15df94cb447185fce03115692d358
MRoyhanF <froyhan0@gmail.com>
2024-12-12 14:27:37 +0700

    fetch: list city

--- src/modules/city/city.controller.js
+++ src/modules/city/city.controller.js
@@ -1,6 +1,11 @@
+import axios from "axios";
+
 export const index = async (req, res, next) => {
   try {
-    res.edge("pages/city/index");
+    const response = await axios.get("https://airhopper-304285428031.asia-southeast1.run.app/api/v1/cities");
+    const cities = response.data.data;
+
+    res.edge("pages/city/index", { cities });
   } catch (error) {
     next(error);
   }
