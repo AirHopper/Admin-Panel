de99b67384821cbbcc748315bc0c35efd63842c3
MRoyhanF <froyhan0@gmail.com>
2024-12-15 04:35:25 +0700

    featch : flight | detail flights

--- src/modules/flight/flight.controller.js
+++ src/modules/flight/flight.controller.js
@@ -1,6 +1,25 @@
+import axios from 'axios';
+
 export const index = async (req, res, next) => {
   try {
-    res.edge("pages/flight/index");
+    const api = process.env.API_URL;
+    
+    const flightsResponse = await axios.get(`${api}/api/v1/flights?limit=100`);
+
+    const data = {
+      api: api,
+      flights: flightsResponse.data.data || [],
+    };
+
+    res.edge("pages/flight/index", data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const detail = async (req, res, next) => {
+  try {
+    res.edge("pages/flight/detail");
   } catch (error) {
     next(error);
   }
--- src/modules/flight/flight.routes.js
+++ src/modules/flight/flight.routes.js
@@ -3,4 +3,5 @@ import * as flightController from "./flight.controller.js";
 export default (router) => {
   const prefix = "/flight";
   router.get(prefix + "/", flightController.index);
+  router.get(prefix + "/:id", flightController.detail);
 };
