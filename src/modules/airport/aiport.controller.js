9d73d131bf4806f60ee8d5be4af4dc56110f07c0
MRoyhanF <froyhan0@gmail.com>
2024-12-15 02:42:59 +0700

    featch: airport | get all airport

--- src/modules/airport/aiport.controller.js
+++ src/modules/airport/aiport.controller.js
@@ -1,6 +1,18 @@
+import axios from "axios";
+
 export const index = async (req, res, next) => {
   try {
-    res.edge("pages/airport/index");
+    const api = process.env.API_URL;
+    
+    const citiesResponse = await axios.get(`${api}/api/v1/cities`);
+    const airportsResponse = await axios.get(`${api}/api/v1/airports`);
+
+    const data = {
+      cities: citiesResponse.data.data || [],
+      airports: airportsResponse.data.data || [],
+    };
+
+    res.edge("pages/airport/index", data);
   } catch (error) {
     next(error);
   }
