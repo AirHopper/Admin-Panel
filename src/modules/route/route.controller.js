1a12ef38d542fc4164ae169d6d7fc2d55a5cea94
MRoyhanF <froyhan0@gmail.com>
2024-12-15 07:27:34 +0700

    feat: route | create route

--- src/modules/route/route.controller.js
+++ src/modules/route/route.controller.js
@@ -1,6 +1,19 @@
+import axios from "axios";
+
 export const index = async (req, res, next) => {
   try {
-    res.edge("pages/route/index");
+    const api = process.env.API_URL;
+    
+    const citiesResponse = await axios.get(`${api}/api/v1/cities`);
+    const routesResponse = await axios.get(`${api}/api/v1/routes`);
+
+    const data = {
+      api: api,
+      cities: citiesResponse.data.data || [],
+      routes: routesResponse.data.data || [],
+    };
+
+    res.edge("pages/route/index", data);
   } catch (error) {
     next(error);
   }
