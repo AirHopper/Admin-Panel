411b27009b68d487211f9db8b82df0ddf70737a0
MRoyhanF <froyhan0@gmail.com>
2024-12-17 16:12:29 +0700

    featch: flight | featching detail flight

--- src/modules/flight/flight.controller.js
+++ src/modules/flight/flight.controller.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+const formatRupiah = (value) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(value);
+};
+
+
 export const index = async (req, res, next) => {
   try {
     const api = process.env.API_URL;
@@ -19,7 +27,22 @@ export const index = async (req, res, next) => {
 
 export const detail = async (req, res, next) => {
   try {
-    res.edge("pages/flight/detail");
+    const api = process.env.API_URL;
+    const id = req.params.id;
+
+    const flightDetailResponse = await axios.get(`${api}/api/v1/flights/${id}`);
+    const flight = {
+      ...flightDetailResponse.data.data,
+      price: formatRupiah(flightDetailResponse.data.data.price),
+      totalPrice: formatRupiah(flightDetailResponse.data.data.totalPrice),
+    };
+
+    const data = {
+      api: api,
+      flight,
+    };
+
+    res.edge("pages/flight/detail", data);
   } catch (error) {
     next(error);
   }
