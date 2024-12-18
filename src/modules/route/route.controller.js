162bbce7f32fbe5af5cc06908a7b2fb14a86d977
MRoyhanF <froyhan0@gmail.com>
2024-12-18 22:23:41 +0700

    update : routes | feathing by token

--- src/modules/route/route.controller.js
+++ src/modules/route/route.controller.js
@@ -3,13 +3,18 @@ import axios from "axios";
 export const index = async (req, res, next) => {
   try {
     const api = process.env.API_URL;
-    
+
+    const { token } = req;
+
     const citiesResponse = await axios.get(`${api}/api/v1/cities`);
+    const airportsResponse = await axios.get(`${api}/api/v1/airports`);
     const routesResponse = await axios.get(`${api}/api/v1/routes`);
 
     const data = {
       api: api,
+      token: token,
       cities: citiesResponse.data.data || [],
+      airports: airportsResponse.data.data || [],
       routes: routesResponse.data.data || [],
     };
 
