c2b829567f46d61cd2a8d5a08ad1191a61e9a6f6
MRoyhanF <froyhan0@gmail.com>
2024-12-18 19:21:49 +0700

    feat: users | list users and create new admin account

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -3,19 +3,21 @@ import axios from 'axios';
 export const index = async (req, res, next) => {
     try {
         const api = process.env.API_URL;
-        const { token, email, role } = req;
+        const { token } = req;
 
-        const users = await axios.get(`${api}/api/v1/users`, {
+        const usersResponse = await axios.get(`${api}/api/v1/users`, {
             headers: {
               Authorization: `Bearer ${token}`,
             },
           });
         
         const data = {
-            totalUsers : users.data.data.length,
+            token : token,
+            api : api,
+            users : usersResponse.data.data || [],
         }
 
-        res.edge('pages/user/index', { data, email, role });
+        res.edge('pages/user/index', data);
     } catch (error) {
         next(error)
     }
