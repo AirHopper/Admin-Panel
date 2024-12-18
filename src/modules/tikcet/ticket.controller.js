a9d88e57f5952a40db3aa27195d326cb7315730c
MRoyhanF <froyhan0@gmail.com>
2024-12-18 13:07:41 +0700

    feat:ticket | list ticket

--- src/modules/tikcet/ticket.controller.js
+++ src/modules/tikcet/ticket.controller.js
@@ -1,6 +1,17 @@
+import axios from "axios";
+
 export const index = async (req, res, next) => {
   try {
-    res.edge("pages/ticket/index");
+    const api = process.env.API_URL;
+
+    const tiketResponse = await axios.get(`${api}/api/v1/tickets?limit=999999`);  
+
+    const data = {
+      api: api,
+      tickets: tiketResponse.data.data || [],
+    };
+
+    res.edge("pages/ticket/index", data);
   } catch (error) {
     next(error);
   }
