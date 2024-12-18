150d1890664f733072dd2180c518e145319e32c4
MRoyhanF <froyhan0@gmail.com>
2024-12-18 22:28:23 +0700

    update : airlines | feathing by token

--- src/modules/airline/airline.controller.js
+++ src/modules/airline/airline.controller.js
@@ -3,11 +3,19 @@ import axios from "axios";
 export const index = async (req, res, next) => {
   try {
     const api = process.env.API_URL;
+
+    const { token } = req;
     
     const response = await axios.get(`${api}/api/v1/airlines`);
     const airlines = response.data.data;
 
-    res.edge("pages/airline/index", { airlines, api });
+    const data = {  
+      api: api,
+      token: token,
+      airlines: airlines,
+    };
+
+    res.edge("pages/airline/index", data);
   } catch (error) {
     next(error);
   }
@@ -17,11 +25,13 @@ export const listAirplanes = async (req, res, next) => {
   try {
     const api = process.env.API_URL;
     const code = req.params.code;
+    const { token } = req;
 
     const airlinesResponse = await axios.get(`${api}/api/v1/airlines/${code}`);
 
     const data = {
       api: api,
+      token: token,
       airlines: airlinesResponse.data.data,
     };
 
