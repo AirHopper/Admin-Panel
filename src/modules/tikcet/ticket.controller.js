2bd1b933e9679866a0ff45a72e1308d257daaa7e
MRoyhanF <froyhan0@gmail.com>
2024-12-18 13:30:12 +0700

    feat: ticket | setup page for detail ticket

--- src/modules/tikcet/ticket.controller.js
+++ src/modules/tikcet/ticket.controller.js
@@ -16,3 +16,21 @@ export const index = async (req, res, next) => {
     next(error);
   }
 };
+
+export const detail = async (req, res, next) => {
+  try {
+    const api = process.env.API_URL;
+    const id = req.params.id;
+
+    const ticketDetailResponse = await axios.get(`${api}/api/v1/tickets/${id}`);
+
+    const data = {  
+      api: api,
+      ticket: ticketDetailResponse.data.data || {},
+    };
+
+    res.edge("pages/ticket/detail", data);
+  } catch (error) {
+    next(error);
+  }
+};
