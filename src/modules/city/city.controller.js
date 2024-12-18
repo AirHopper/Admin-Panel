eae500500b17931c26aa822deeefb37ca30c15a4
MRoyhanF <froyhan0@gmail.com>
2024-12-18 21:21:33 +0700

    update : cities | feathing by token

--- src/modules/city/city.controller.js
+++ src/modules/city/city.controller.js
@@ -3,10 +3,12 @@ import axios from "axios";
 export const index = async (req, res, next) => {
   try {
     const api = process.env.API_URL;
+    const { token } = req;
     
     const citiesResponse = await axios.get(`${api}/api/v1/cities`);
     
     const data = {
+      token: token,
       api: api,
       cities: citiesResponse.data.data || [],
     };  
