da7bd6084e09857f6fbd6dd8049375690b0548ba
MRoyhanF <froyhan0@gmail.com>
2024-12-19 22:58:31 +0700

    feat: ticket | list flight available at the time

--- src/modules/tikcet/ticket.controller.js
+++ src/modules/tikcet/ticket.controller.js
@@ -52,8 +52,36 @@ export const detail = async (req, res, next) => {
 
 export const create = async (req, res, next) => {
   try {
+    const api = process.env.API_URL;
+    const token = req.cookies.token;
+    const routeId = req.query.routeId;
+    const departureDate = req.query.departureDate;
 
-    res.edge("pages/ticket/create");
+    const routeResponse = await axios.get(`${api}/api/v1/routes/${routeId}`);
+    const departureAirport = routeResponse.data.data.departureAirportId;
+    const arrivalAirport = routeResponse.data.data.arrivalAirportId;
+
+    let flightDepartureData = [];
+    let flightArrivalData = [];
+
+    if (departureDate) {
+      const flightDepartureResponse = await axios.get(`${api}/api/v1/flights?limit=999999&search[departureAirport]=${departureAirport}&search[flightDate]=${departureDate}`);
+      flightDepartureData = flightDepartureResponse.data.data;
+
+      const flightArrivalResponse = await axios.get(`${api}/api/v1/flights?limit=999999&search[arrivalAirport]=${arrivalAirport}&search[flightDate]=${departureDate}`);
+      flightArrivalData = flightArrivalResponse.data.data;
+    }
+
+    const data = {
+      api: api,
+      token: token,
+      departureDate: departureDate,
+      route: routeResponse.data.data,
+      flightDepartureData,
+      flightArrivalData,
+    };
+
+    res.edge("pages/ticket/create", data);
   } catch (error) {
     next(error);
   }
